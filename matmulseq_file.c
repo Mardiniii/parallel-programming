c35192860e725ea3ef654478a86084690be6c1c5
mardiniii <sezama03@hotmail.com>
2016-06-10 17:07:50 -0500

    Innitial Commit

---
+++ matmulseq_file.c
@@ -0,0 +1,101 @@
+//Matrix multiplication
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double **a, **b, **c;
+int matrixSize;
+
+double **allocateMatrix() {
+	int i;
+	double *vals, **temp;
+
+	// allocate space for values
+	vals = (double *) malloc (matrixSize * matrixSize * sizeof(double));
+
+	// allocate vector of pointers
+	temp = (double **) malloc (matrixSize * sizeof(double*));
+
+	for(i=0; i < matrixSize; i++)
+		temp[i] = &(vals[i * matrixSize]);
+
+	return temp;
+}
+
+void mm(void) {
+	int i,j,k;
+	double sum;
+
+	for (i = 0; i < matrixSize; i++) {
+		for (j = 0; j < matrixSize; j++) {
+			sum = 0.0;
+			for (k = 0; k < matrixSize; k++) {
+				sum = sum + a[i][k] * b[k][j];
+			}
+			c[i][j] = sum;
+		}
+	}
+}
+void printResult(void){
+	int i, j;
+	for(i=0;i<matrixSize;i++){
+		for(j=0;j<matrixSize;j++){
+			printf("%lf ", c[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main(void) {
+	int i, j, k;
+	int nmats;
+	char *fname = "matrices_test.dat"; //Change to matrices_large.dat for performance evaluation
+	FILE *fh;
+
+	printf("Start\n");
+	fh = fopen(fname, "r");
+	//First line indicates how many pairs of matrices there are and the matrix size
+	fscanf(fh, "%d %d\n", &nmats, &matrixSize);
+
+	//Dynamically create matrices of the size needed
+	a = allocateMatrix();
+	b = allocateMatrix();
+	c = allocateMatrix();
+
+	printf("Loading %d pairs of square matrices of size %d from %s...\n", nmats, matrixSize, fname);
+	clock_t start = clock();
+	for(k=0;k<nmats;k++){
+		for(i=0;i<matrixSize;i++){
+			for(j=0;j<matrixSize;j++){
+				fscanf(fh, "%lf", &a[i][j]);
+			}
+		}
+		for(i=0;i<matrixSize;i++){
+			for(j=0;j<matrixSize;j++){
+				fscanf(fh, "%lf", &b[i][j]);
+			}
+		}
+
+		printf("Multiplying two matrices...\n");
+		mm();
+		printResult();
+	}
+	clock_t end = clock();
+	printf("************* \n");
+	double time_elapsed_in_seconds = (end - start)/(double)CLOCKS_PER_SEC;
+	printf("The time for the operation was %f \n", time_elapsed_in_seconds);
+	printf("************* \n");
+	fclose(fh);
+
+	// Free memory
+	free(*a);
+	free(a);
+	free(*b);
+	free(b);
+	free(*c);
+	free(c);
+	printf("Done.\n");
+	return 0;
+}
+
