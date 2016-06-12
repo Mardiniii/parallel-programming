9ff7c455eb2b9236a6bc8adc3dcd7818b79ed51d
mardiniii <sezama03@hotmail.com>
2016-06-12 11:37:21 -0500

    Add case for threads equal to one and for matrix size % threads = 0

--- version_1.c
+++ version_1.c
@@ -6,7 +6,15 @@
 #include <time.h>
 
 // Define number of threads
-#define NUM_THREADS     4
+#define NUM_THREADS     10
+
+struct thread_data{
+   int thread_id;
+   int initial_row;
+   int final_row;
+};
+
+struct thread_data thread_data_array[NUM_THREADS];
 
 double **a, **b, **c;
 int matrixSize;
@@ -27,13 +35,17 @@ double **allocateMatrix() {
   return temp;
 }
 
-void *thread_operation(void *row) {
-  int j,k,i;
+void *thread_operation(void *threadarg) {
+  int j,k,i,initial_row,final_row;
+  struct thread_data *my_data;
+  long t_id;
   double sum;
-  long initial_row;
-  initial_row = (long)row;
-  printf("Hola soy el Thread #%ld, multiplicando la fila #%ld!\n", initial_row, initial_row );
-  for (i = initial_row; i < initial_row + 1; i++) {
+  my_data = (struct thread_data *) threadarg;
+  t_id = my_data->thread_id;
+  initial_row = my_data->initial_row;
+  final_row = my_data->final_row;
+  printf("Hola soy el Thread #%ld, multiplicando desde la fila #%d hasta la fila #%d!\n", t_id, initial_row, final_row);
+  for (i = initial_row; i < final_row; i++) {
     for (j = 0; j < matrixSize; j++) {
       sum = 0.0;
       for (k = 0; k < matrixSize; k++) {
@@ -46,32 +58,56 @@ void *thread_operation(void *row) {
 }
 
 void mm(void) {
-  int i,j,k,rc;
+  int i,j,k;
   double sum;
-  void *status;
 
+  for (i = 0; i < matrixSize; i++) {
+    for (j = 0; j < matrixSize; j++) {
+      sum = 0.0;
+      for (k = 0; k < matrixSize; k++) {
+        sum = sum + a[i][k] * b[k][j];
+      }
+      c[i][j] = sum;
+    }
+  }
+}
+
+void parallel_multiplication(void) {
+  int i,j,k,rc,step;
+  double sum;
+  long t;
+  void *status;
   pthread_t threads[NUM_THREADS];
   pthread_attr_t attr;
-  long t;
   /* Initialize and set thread detached attribute */
   pthread_attr_init(&attr);
   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-  for(t=0; t<NUM_THREADS; t++){
-    rc = pthread_create(&threads[t], &attr, thread_operation,(void *)t);
-    if (rc){
-      printf("ERROR; return code from pthread_create() is %d\n", rc);
-      exit(-1);
+  // Asignar hilos como filas tenga la matriz
+  // numero de filas/numero de threads
+  // First case when the number of threads is equal to one
+  if( NUM_THREADS == 1 ) {
+    mm();
+  } else if ( (matrixSize % NUM_THREADS) == 0 ) {
+    step = matrixSize/NUM_THREADS;
+    for(t=0; t<NUM_THREADS; t++){
+      thread_data_array[t].thread_id = t;
+      thread_data_array[t].initial_row = t * step;
+      thread_data_array[t].final_row = (t+1)*step;
+      rc = pthread_create(&threads[t], &attr, thread_operation,(void *) &thread_data_array[t]);
+      if (rc){
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        exit(-1);
+      }
     }
-  }
-
-  /* Free attribute and wait for the other threads */
-  pthread_attr_destroy(&attr);
-  for(t=0; t<NUM_THREADS; t++) {
-    rc = pthread_join(threads[t], &status);
-    if (rc) {
-      printf("ERROR; return code from pthread_join() is %d\n", rc);
-      exit(-1);
+    /* Free attribute and wait for the other threads */
+    pthread_attr_destroy(&attr);
+    for(t=0; t<NUM_THREADS; t++) {
+      rc = pthread_join(threads[t], &status);
+      if (rc) {
+        printf("ERROR; return code from pthread_join() is %d\n", rc);
+        exit(-1);
+      }
     }
   }
 }
@@ -89,7 +125,7 @@ void printResult(void){
 int main(void) {
   int i, j, k;
   int nmats;
-  char *fname = "matrices_test.dat"; //Change to matrices_large.dat for performance evaluation
+  char *fname = "matrices_large.dat"; //Change to matrices_large.dat for performance evaluation
   FILE *fh;
 
   printf("Start\n");
@@ -117,7 +153,7 @@ int main(void) {
     }
 
     printf("Multiplying two matrices...\n");
-    mm();
+    parallel_multiplication();
     printResult();
   }
   clock_t end = clock();
