bcbb01347f9f011830aa5d182d56ff320906d7a7
mardiniii <sezama03@hotmail.com>
2016-06-13 18:30:50 -0500

    Add funcionality to create the result matrix structure

--- version_2.c
+++ version_2.c
@@ -9,15 +9,21 @@
 #include <time.h>
 
 // Define number of threads
-#define NUM_THREADS     2
+#define NUM_THREADS     4
 
-double **a, **b, **c;
 int matrixSize;
-int operation_counter;
+int operation_counter = 0;
+
+struct  results  {
+  int  id;
+  double  **matrix_c;
+};
+
+struct results matrix_result[50];
 
 // Global definitios as document given by the professor
+pthread_mutex_t mutex_matrix_multiplication = PTHREAD_MUTEX_INITIALIZER;
 pthread_t threads[NUM_THREADS];
-pthread_mutex_t mutex_matrix_multiplication;
 
 double **allocateMatrix() {
   int i;
@@ -35,54 +41,68 @@ double **allocateMatrix() {
   return temp;
 }
 
-void *thread_operation(void *threadid) {
-  int i,j,k;
-  double sum;
-
-  for (i = 0; i < matrixSize; i++) {
-    for (j = 0; j < matrixSize; j++) {
-      sum = 0.0;
-      for (k = 0; k < matrixSize; k++) {
-        sum = sum + a[i][k] * b[k][j];
-      }
-      c[i][j] = sum;
-    }
-  }
-  pthread_mutex_lock( &mutex_matrix_multiplication );
-  operation_counter++;
-  pthread_mutex_unlock( &mutex_matrix_multiplication );
-  // pthread_exit((void*) 0);
-}
-
-void mm(void) {
-  int i,j,k;
-  double sum;
-
-  for (i = 0; i < matrixSize; i++) {
-    for (j = 0; j < matrixSize; j++) {
-      sum = 0.0;
-      for (k = 0; k < matrixSize; k++) {
-        sum = sum + a[i][k] * b[k][j];
-      }
-      c[i][j] = sum;
-    }
-  }
-}
-
-void printResult(void){
+// This function is edited to show one result matrix from the structure sending the id
+void printResult(int result_id){
   int i, j;
+  printf("*************************\n");
+  printf("Printing result #%d...\n",result_id+1);
+  printf("*************************\n");
   for(i=0;i<matrixSize;i++){
     for(j=0;j<matrixSize;j++){
-      printf("%lf ", c[i][j]);
+      printf("%lf ", matrix_result[result_id].matrix_c[i][j]);
     }
     printf("\n");
   }
 }
 
+void *thread_operation(void *threadid) {
+  int i,j,k;
+  double sum;
+  double **a, **b;
+  printf("Start\n");char *fname = "matrices_large.dat"; //Change to matrices_large.dat for performance evaluation
+  FILE *fh;
+
+  printf("Start\n");
+  fh = fopen(fname, "r");
+  //First line indicates how many pairs of matrices there are and the matrix size
+  a = allocateMatrix();
+  b = allocateMatrix();
+  for(i=0;i<matrixSize;i++){
+    for(j=0;j<matrixSize;j++){
+      fscanf(fh, "%lf", &a[i][j]);
+    }
+  }
+  for(i=0;i<matrixSize;i++){
+    for(j=0;j<matrixSize;j++){
+      fscanf(fh, "%lf", &b[i][j]);
+    }
+  }
+  for (i = 0; i < matrixSize; i++) {
+    for (j = 0; j < matrixSize; j++) {
+      sum = 0.0;
+      for (k = 0; k < matrixSize; k++) {
+        sum = sum + a[i][k] * b[k][j];
+      }
+      matrix_result[operation_counter].matrix_c[i][j] = sum;
+      // c[i][j] = sum;
+    }
+  }
+
+  printf("Multiplying two matrices...\n");
+  pthread_mutex_lock( &mutex_matrix_multiplication );
+  operation_counter++;
+  pthread_mutex_unlock( &mutex_matrix_multiplication );
+
+  printf("Multiplying two matrices...\n");
+  // printResult(matrix_result[operation_counter]);
+  pthread_exit(NULL);
+}
+
 int main(void) {
   int i, j, k;
   long t;
-  int nmats;
+  int nmats, step;
+  int operate = 1;
   char *fname = "matrices_large.dat"; //Change to matrices_large.dat for performance evaluation
   FILE *fh;
 
@@ -91,45 +111,43 @@ int main(void) {
   //First line indicates how many pairs of matrices there are and the matrix size
   fscanf(fh, "%d %d\n", &nmats, &matrixSize);
 
-  //Dynamically create matrices of the size needed
-  a = allocateMatrix();
-  b = allocateMatrix();
-  c = allocateMatrix();
+  // Dynamically create matrices of the size needed for the result structure
+  for(k=0;k<nmats;k++){
+    printf("Creating result matrix #%d\n", k+1);
+    matrix_result[k].matrix_c=allocateMatrix();
+  }
+  printResult(49);
 
-  printf("Loading %d pairs of square matrices of size %d from %s...\n", nmats, matrixSize, fname);
   clock_t start = clock();
-
-  if( NUM_THREADS == 1 ) {
-    // Run the normal matrix multiplication function
-    mm();
-  // Case 2
-  } else if ( (matrixSize % NUM_THREADS) == 0 ) {
-    while(operation_counter < nmats){
-      for(k=0;k<nmats;k++){
-        for(i=0;i<matrixSize;i++){
-          for(j=0;j<matrixSize;j++){
-            fscanf(fh, "%lf", &a[i][j]);
-          }
+  step = nmats%NUM_THREADS;
+  while(operate == 1){
+    void *status;
+    int rc;
+    pthread_attr_t attr;
+    /* Initialize and set thread detached attribute */
+    pthread_mutex_init(&mutex_matrix_multiplication, NULL);
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    if (operation_counter == (50-step) ) {
+      for(t=0; t<step; t++){
+        rc = pthread_create(&threads[t], &attr, thread_operation,(void *)t);
+        if (rc){
+          printf("ERROR; return code from pthread_create() is %d\n", rc);
+          exit(-1);
         }
-        for(i=0;i<matrixSize;i++){
-          for(j=0;j<matrixSize;j++){
-            fscanf(fh, "%lf", &b[i][j]);
-          }
-        }
-
-        printf("Multiplying two matrices...\n");
-        mm();
-        printResult();
       }
-      // Create threads as documents given with the initial code
-      void *status;
-      int rc;
-      pthread_attr_t attr;
-      /* Initialize and set thread detached attribute */
-      // pthread_mutex_init(&mutex_matrix_multiplication, NULL);
-      pthread_attr_init(&attr);
-      pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
+      /* Free attribute and wait for the other threads */
+      pthread_attr_destroy(&attr);
+      // Wait for the work of all the threads with the join and continue
+      for(t=0; t<step; t++) {
+        rc = pthread_join(threads[t], &status);
+        if (rc) {
+          printf("ERROR; return code from pthread_join() is %d\n", rc);
+          exit(-1);
+        }
+      }
+      operate = 0;
+    } else {
       for(t=0; t<NUM_THREADS; t++){
         rc = pthread_create(&threads[t], &attr, thread_operation,(void *)t);
         if (rc){
@@ -148,7 +166,6 @@ int main(void) {
         }
       }
     }
-  // Case 3
   }
   clock_t end = clock();
   printf("************* \n");
@@ -158,12 +175,11 @@ int main(void) {
   fclose(fh);
 
   // Free memory
-  free(*a);
-  free(a);
-  free(*b);
-  free(b);
-  free(*c);
-  free(c);
+  // // Free memory
+   for(k=0;k<nmats;k++){
+    free(*matrix_result[k].matrix_c);
+    free(matrix_result[k].matrix_c);
+   }
   printf("Done.\n");
   // pthread_mutex_destroy(&mutex_matrix_multiplication);
   // pthread_exit(NULL);
